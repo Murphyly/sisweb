ea5e27e48ffedaec39b1acf10c1c3ad57058c3d9
Murphyly <isysnogueira@infojr.com.br>
2019-11-03 20:01:33 -0300

    finaliza xml

--- assets/js/membros.js
+++ assets/js/membros.js
@@ -4,14 +4,14 @@ function getData(i) {
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("content").innerHTML =
-        this.responseText;
-        var table = " ";
+        document.getElementById("members").innerHTML = this.responseText;
+        var section = " ";
         var datas = this.responseXML.getElementsByTagName("HEROI");
         for(i = 0; i < datas.length; i++)
-        table += '<tr><td>' + datas[i].getElementsByTagName("img")[0].childNodes[0].nodeValue
-        + '</td><td>' + datas[i].getElementsByTagName("NAME")[0].childNodes[0].nodeValue + "</td></tr>";
-        document.getElementById("content").innerHTML = table;
+        section += '<article><div><a href="' + datas[i].getElementsByTagName("linkheroi")[0].childNodes[0].nodeValue + 
+        '"><img src="' + datas[i].getElementsByTagName("img")[0].childNodes[0].nodeValue
+        + '"></a></div><hr><h2>' + datas[i].getElementsByTagName("NAME")[0].childNodes[0].nodeValue + "</h2></article>";
+        document.getElementById("members").innerHTML = section;
     }
      };
      xmlhttp.open("GET", "/assets/xml/membro.xml", true); 
